d2ba44a44acba638d7d1c82e9a29831323af5a28
Abdoulaye Diallo <shotone@icloud.com>
2015-02-01 12:31:42 -0500

    Initial Commit

---
+++ TestChallengeTSU/MasterViewController.h
@@ -0,0 +1,15 @@
+//
+//  MasterViewController.h
+//  TestChallengeTSU
+//
+//  Created by A's macAir on 2/1/15.
+//  Copyright (c) 2015 Abdoulaye Diallo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+
+@end
+
